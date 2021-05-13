ed1d8cb8bc093393e59a9b186b996b9db9b97691
Fraborcar1 <80266204+Fraborcar1@users.noreply.github.com>
2021-05-13 17:43:25 +0200

    Add files via upload
    Creo que con esto ya está player

--- src/main/java/aiss/model/Player.java
+++ src/main/java/aiss/model/Player.java
@@ -1,5 +1,45 @@
 package aiss.model;
 
 public class Player {
+	private String id;
+	private String name;
+	private Integer rating;
+	
+	public Player () {
+	}
+	
+	public Player(String name, Integer rating) {
+		this.name = name;
+		this.rating = rating;
+	}
+	
+	public Player(String id, String name, Integer rating) {
+		this.id = id;
+		this.name = name;
+		this.rating = rating;
+	}
+	
+	public String getId() {
+		return id;
+	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
 }
