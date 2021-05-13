0a08e5e6d70d594d292a7b1923801fc72ca13cb1
Jesús Páez <jespaepae@alum.us.es>
2021-05-13 19:24:08 +0200

    solucionando problemas con merge

---
+++ src/main/java/aiss/api/resources/comparators/ComparatorRatingPlayerReversed.java
@@ -0,0 +1,14 @@
+package aiss.api.resources.comparators;
+
+import java.util.Comparator;
+
+import aiss.model.Player;
+
+public class ComparatorRatingPlayerReversed implements Comparator<Player>{
+
+	@Override
+	public int compare(Player p1, Player p2) {
+		return - Integer.compare(Integer.parseInt(p1.getName()), Integer.parseInt(p2.getName()));
+	}
+	
+}
